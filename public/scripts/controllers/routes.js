7037eba5e521a321e6a26683ff39db116b79d04f
Keith Dahlby <keith@newbo.co>
2017-07-24 13:22:34 -0500

    Add active link middleware

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -1,5 +1,14 @@
 'use strict';
 // This is where we define the client-side routes
+page('*', //views.middleware.activeHeaderLink);
+  function (ctx, next) {
+    var activeLink = $(`a[href='${ctx.path}']`);
+
+    $('a.active').removeClass('active');
+    activeLink.toggleClass('active');
+
+    next();
+  });
 
 page('/', views.home);
 page('/about', views.about);
